c3068e46b7ecb355d1183bc76cbc825d7d6e6638
Marcel Canhisares <marcelcanhisares@MacBook-Air-de-Karla.local>
2016-01-08 14:09:25 -0200

    First commit, learning auto-layout and doing exercise

---
+++ auto-layout-advanced/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  auto-layout-advanced
+//
+//  Created by Marcel Canhisares on 07/01/16.
+//  Copyright © 2016 Azell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
